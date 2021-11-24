e917b9de4483d4f70e19858408aee335e6882ac4
Emily <emilybribragg@gmail.com>
2021-11-24 12:50:19 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+const companyName = 'Scuber';
+let mostProfitableNeighborhood;
+mostProfitableNeighborhood='Chelsea';
+let companyCeo;
+companyCeo='Susan Smith';
